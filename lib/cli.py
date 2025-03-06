8ecf99199d423b904089bcb3673f0e6f3fd3043d
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-03-06 15:48:48 -0500

    Updated project

--- lib/cli.py
+++ lib/cli.py
@@ -3,63 +3,72 @@
 from helpers import (
     exit_program,
     interact_with_brand_data,
-    interact_with_car_model_data
+    interact_with_car_model_data,
+    create_animals_table
 )
 
 
 def main():
     while True:
-        # print("1.Car Brand & Model Catalog")
-        # print("2.Add a New Car Brand")
-        # print("View All Car Brands")
-        # print("Add a Model to a Brand")
-        # print("View Models for a Brand")
-        # print("Delete a Brand")
-        # print("Exit")
+        menu()
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            name = input("Enter brand name: ")
+            country = input("Enter country of origin: ")
+            Brand.add_brand(name, country)
 
-        # choice = input("Enter your choice: ")
+        elif choice == "2":
+            Brand.get_all()
 
-        # if choice == "1":
-        #     name = input("Enter brand name: ").strip()
-        #     country = input("Enter country of origin: ").strip()
-        #     Brand.add_brand(name, country)
+        elif choice == "3":
+            Brand.get_all()
+            brand_id = input("Enter Brand ID to add a model: ")
+            model_name = input("Enter model name: ")
+            year = input("Enter model year: ")
 
-        # elif choice == "2":
-        #     Brand.get_all_brands()
+            if brand_id.isdigit() and year.isdigit():
+                CarModel.create(int(brand_id), model_name, int(year))
+            else:
+                print("Invalid input! Brand ID and Year must be numbers.")
 
-        # elif choice == "3":
-        #     Brand.get_all_brands()
-        #     brand_id = input("Enter Brand ID to add a model: ").strip()
-        #     model_name = input("Enter model name: ").strip()
-        #     year = input("Enter model year: ").strip()
+        elif choice == "4":
+            Brand.get_all()
+            brand_id = input("Enter Brand ID to view models: ").strip()
 
-        #     if brand_id.isdigit() and year.isdigit():
-        #         CarModel.add_model(int(brand_id), model_name, int(year))
-        #     else:
-        #         print("Invalid input! Brand ID and Year must be numbers.")
+            if brand_id.isdigit():
+                CarModel.get_models_by_id(int(brand_id))
+            else:
+                print("Invalid input! Brand ID must be a number.")
 
-        # elif choice == "4":
-        #     Brand.get_all_brands()
-        #     brand_id = input("Enter Brand ID to view models: ").strip()
+        elif choice == "5":
+            Brand.get_all()
+            brand_id = input("Enter Brand ID to delete: ")
 
-        #     if brand_id.isdigit():
-        #         CarModel.get_models_by_brand(int(brand_id))
-        #     else:
-        #         print("Invalid input! Brand ID must be a number.")
+            if brand_id.isdigit():
+                Brand.delete_brand(int(brand_id))
+            else:
+                print("Invalid input! Brand ID must be a number.")
 
-        # elif choice == "5":
-        #     Brand.get_all_brands()
-        #     brand_id = input("Enter Brand ID to delete: ").strip()
+        elif choice == "8":
+            create_animals_table()
 
-        #     if brand_id.isdigit():
-        #         Brand.delete_brand(int(brand_id))
-        #     else:
-        #         print("Invalid input! Brand ID must be a number.")
+        elif choice == "0":
+            print("Exiting the catalog. Goodbye!")
+            conn.close()
+            break
 
-        # elif choice == "0":
-        #     print("Exiting the catalog. Goodbye!")
-        #     conn.close()
-        #     break
+        else:
+            print("Invalid choice! Please enter a number from the menu.")
 
-        # else:
-        #     print("Invalid choice! Please enter a number from the menu.")
+
+def menu():
+        print("1.Car Brand & Model Catalog")
+        print("2.Add a New Car Brand")
+        print("3.View All Car Brands")
+        print("4.Add a Model to a Brand")
+        print("5.View Models for a Brand")
+        print("6.Delete a Brand")
+        print("7.Exit")
+        print("8.Create animals table")
+
+
--- lib/helpers.py
+++ lib/helpers.py
@@ -2,6 +2,7 @@
 
 from models.brand import Brand
 from models.car_model import CarModel
+from models.animal import Animal
 
 def interact_with_brand_data():
     print("Performing useful function#1.")
@@ -15,7 +16,9 @@ def interact_with_car_model_data():
     for car_model in CarModel.all:
         print(car_model)
 
-
+def create_animals_table():
+    Animal.create_table()
+    print("Animals table created!")
 
 def exit_program():
     print("Goodbye!")
